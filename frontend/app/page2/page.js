54bcbee508bc4b0467f4c2a6f08c6a73408fd93c
manjaeng <manjaeng@korea.com>
2025-09-04 17:03:15 +0900

    init font api

--- frontend/app/page2/page.js
+++ frontend/app/page2/page.js
@@ -5,7 +5,7 @@ export default function Home() {
     const [message, setMessage] = useState('');
 
     useEffect(() => {
-        fetch('http://127.0.0.1:8000/api/hello/')
+        fetch('https://manjaeng.duckdns.org//api/hello/')
             .then(res => res.json())
             .then(data => setMessage(data.message))
             .catch(err => console.error(err));
